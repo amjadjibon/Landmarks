5d2c701b4515b83fd1d926f3dab480539a3a0165
Amjad Jibon <amjad.jibon@gmail.com>
2024-09-21 14:48:52 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Amjad Hossain on 21/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
